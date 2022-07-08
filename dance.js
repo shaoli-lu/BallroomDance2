0923bd5ce8161b0e93b12dce254d1131b6b5d156
shaoli-lu <shaoli_lu@yahoo.com>
2022-07-07 23:22:21 -0500

    initial

---
+++ dance.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('img').mousedown(function (e) {
+        if(e.button == 2) { // right click
+            return false; // do nothing!
+        }
+    });
+});
+
